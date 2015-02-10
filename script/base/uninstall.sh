c03f0b1bfa83d6e08b65887f1721d8e8c2881efc
Jesse McGinnis <jessieelsom@hotmail.com>
2015-02-10 15:44:11 -0500

    Better install script

---
+++ script/base/uninstall.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#
+# delete symlinked files
+
+DOTFILES_ROOT="`pwd`"
+source "${DOTFILES_ROOT}/base/common.sh"
+
+delete_all() {
+  for source in `find $DOTFILES_ROOT -maxdepth 2 -name \*.symlink`
+  do
+    dest="$HOME/.`basename \"${source%.*}\"`"
+    if [ -f $dest ] || [ -d $dest ]
+    then
+      remove_file $dest
+    fi
+  done
+}
