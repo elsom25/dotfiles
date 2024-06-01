ef161b2624c43287da59a197973c46a579b82c35
Jesse McGinnis <accounts@jcmcginnis.com>
2024-06-01 11:07:45 -0400

    Move install stuff to install and keep zshrc lighter

--- script/base/common.sh
+++ script/base/common.sh
@@ -1,37 +1,59 @@
 #!/bin/bash
 
 #
-# common functions
+# logging functions
 
 info() {
-  printf "  [ \033[00;34m..\033[0m ] $1"
-}
-
-user() {
-  printf "\r  [ \033[0;33m?\033[0m ] $1 "
+  printf "  [ \033[00;34m..\033[0m ] %s\n" "$1"
 }
 
 success() {
-  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
+  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] %s\n" "$1"
+}
+
+added() {
+  printf "\r\033[2K  [ \033[00;33mADD\033[0m] %s\n" "$1"
 }
 
 fail() {
-  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
-  echo ''
-  exit
+  local exit_code="${2:-1}"
+  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] %s\n" "$1"
+  exit "$exit_code"
 }
 
-link_file() {
-  ln -s $1 $2
-  success "linked $2 to $1"
-}
+#
+# file functions
 
 remove_file() {
-  rm -rf $1
+  rm -rf "$1"
   success "removed $1"
 }
 
-move_file() {
-  mv $1 $2
-  success "moved $1 to $2"
+link_file() {
+  if [ -f "$2" ]; then
+    rm -rf "$2"
+    ln -s "$1" "$2"
+    success "replaced $2 with $1"
+  else
+    ln -s "$1" "$2"
+    success "linked $2 to $1"
+  fi
+}
+
+install_dotfiles() {
+  for source in $(find "$1" -maxdepth 2 -name "*.symlink"); do
+    dest="$HOME/.${source##*/}"
+    dest="${dest%.symlink}"
+
+    link_file "$source" "$dest"
+  done
+}
+
+delete_dotfiles() {
+  for source in $(find "$1" -maxdepth 2 -name "*.symlink"); do
+    dest="$HOME/.${source##*/}"
+    dest="${dest%.symlink}"
+
+    remove_file "$dest"
+  done
 }
